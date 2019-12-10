9b902c0a5b5597d7956a4f1297b003cc940cf8e9
mechanicznapomarancza <kzawadka@gmail.com>
2019-12-10 03:23:47 +0100

    Tried 2nd part of 4/25

--- src/day4/Day4.java
+++ src/day4/Day4.java
@@ -11,7 +11,11 @@ public class Day4 {
         for (int i = LOWEST; i <= HIGHEST; i++) {
             String s = Integer.toString(i);
             if (notDecreasingTest(s)) {
-                if (doubleNumberTest(s)) totalNumbers += 1;
+                if (doubleNumberTestV2(s)) {
+                    totalNumbers += 1;
+                    System.out.println(s);
+                }
+
             }
         }
         System.out.println(totalNumbers);
@@ -29,5 +33,26 @@ public class Day4 {
             if (s.charAt(i) == s.charAt(i + 1)) return true;
         }
         return false;
+    }
+
+    public static Boolean doubleNumberTestV2(String s) {
+        int[] count = new int[10];
+        boolean positive = false;
+        for (int i = 0; i < CODE_LENGTH - 1; i++) {
+            int val = Integer.parseInt(String.valueOf(s.charAt(i)));
+            count[val] += 1;
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                positive = true;
+                if (i == CODE_LENGTH - 2) count[val] += 1;
+            } else if (!positive) count[val] -= 1;
+
+        }
+        int repeat2 = 0;
+        int repeatMore = 0;
+        for (int i = 0; i < 10; i++) {
+            if (count[i] == 2) repeat2 = i;
+            if (count[i] > 2) repeatMore = i;
+        }
+        return repeat2 >= repeatMore;
     }
 }
