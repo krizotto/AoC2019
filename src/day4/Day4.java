f53d5d3ac3744b0e6202694f03631d81701c053d
Krzysztof Zawadka <krzysztof.zawadka@pl.markant.com>
2019-12-10 12:59:25 +0100

    4/25 2nd part done

--- src/day4/Day4.java
+++ src/day4/Day4.java
@@ -1,8 +1,6 @@
 package day4;
 
 public class Day4 {
-    private static final int CODE_LENGTH = 6;
-
     public static void main(String[] args) {
         final int LOWEST = 353096;
         final int HIGHEST = 843212;
@@ -10,35 +8,26 @@ public class Day4 {
         int totalNumbers = 0;
         for (int i = LOWEST; i <= HIGHEST; i++) {
             String s = Integer.toString(i);
-            if (notDecreasingTest(s)) {
-                if (doubleNumberTestV2(s)) {
-                    totalNumbers += 1;
-                    System.out.println(s);
-                }
-
-            }
+            if (notDecreasingTest(s) && doubleNumberTest(s)) totalNumbers += 1;
         }
-        System.out.println(totalNumbers);
+        System.out.println("Total numbers matching criteria: " + totalNumbers);
     }
 
-    public static Boolean notDecreasingTest(String s) {
-        for (int i = 0; i < CODE_LENGTH - 1; i++) {
+    private static Boolean notDecreasingTest(String s) {
+        for (int i = 0; i < s.length() - 1; i++)
             if (s.charAt(i) > s.charAt(i + 1)) return false;
-        }
         return true;
     }
 
-    public static Boolean doubleNumberTest(String s) {
-        for (int i = 0; i < CODE_LENGTH - 1; i++) {
-            if (s.charAt(i) == s.charAt(i + 1)) return true;
-        }
+    private static Boolean doubleNumberTest(String s) {
+        for (int i = 0; i < s.length() - 1; i++) if (s.charAt(i) == s.charAt(i + 1)) return true;
         return false;
     }
 
-    public static Boolean doubleNumberTestV2(String s) {
-        int[] count = new int[10]; //glowny zamysl - wpisuje w te tablice ile razy powtorzyla sie ta cyfra
+    private static Boolean doubleNumberTestV2(String s) {
+        int[] count = new int[10];
         boolean repeat = false;
-        for (int i = 0; i < CODE_LENGTH - 1; i++) {
+        for (int i = 0; i < s.length() - 1; i++) {
             char current = s.charAt(i);
             char next = s.charAt(i + 1);
             int val = Integer.parseInt(String.valueOf(current));
@@ -50,18 +39,14 @@ public class Day4 {
                 count[val] += 1;
                 repeat = false;
             }
-            if (i == CODE_LENGTH - 2 && repeat) {
+            if (i == s.length() - 2 && repeat) {
                 count[val] += 1;
             }
         }
 
-        int repeat2 = 0;
-        int repeatMore = 0;
         for (int i = 0; i < 10; i++) {
-            if (count[i] == 2) repeat2 = i;
-            if (count[i] > 2) repeatMore = i;
+            if (count[i] == 2) return true;
         }
-        if (repeat2 >= repeatMore && repeat2 != repeatMore) return true;
         return false;
     }
 }
